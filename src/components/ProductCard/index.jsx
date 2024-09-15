72d77cf7e1948cb032bd37ee838fe871675528bf
Guilherme Moretti <guilherme.moretti@abcfitness.com>
2024-09-15 20:51:31 -0300

    qualquer coisa

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,4 +1,5 @@
 const ProductCard = () => {
+<<<<<<< HEAD
   return <div>
       <div className="card card-side bg-base-100 shadow-xl">
           <figure>
@@ -15,6 +16,24 @@ const ProductCard = () => {
           </div>
       </div>
   </div>
+=======
+    return <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure>
+                <img
+                src=""
+                alt="Movie" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">New movie is released!</h2>
+                <p>Click the button to watch on Jetflix app.</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary">Watch</button>
+                </div>
+            </div>
+        </div>
+    </div>
+>>>>>>> d443600 (qualquer coisa)
 }
 
 export default ProductCard
