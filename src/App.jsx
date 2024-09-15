ec178a1f4ce4de110ef19865494cd76e9c0d7f45
Guilherme Moretti <guilherme.moretti@abcfitness.com>
2024-09-15 20:40:32 -0300

    navbar sem funcionar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Catalogo from "./components/pages/Catalogo";
 import Carrinho from "./components/pages/Carrinho";
+import Navbar from "./components/Navbar/index"
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Catalogo />} />
 
