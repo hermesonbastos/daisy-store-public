df8ebc602e39062d878e16bd5efe7c0e8ad026fb
hermesonbastos <hermesonbastos@gmail.com>
2024-09-15 20:50:48 -0300

    Refactorizar importações e corrigir formatação de código

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Catalogo from "./components/pages/Catalogo";
 import Carrinho from "./components/pages/Carrinho";
-import Navbar from "./components/Navbar/index"
+import Navbar from './components/Navbar/index';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Catalogo />} />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Catalogo />} />
 
-          <Route path="/cart" element={<Carrinho />} />
-        </Routes>
-    </>
+        <Route path="/cart" element={<Carrinho />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/pages/Catalogo/index.jsx
+++ src/components/pages/Catalogo/index.jsx
@@ -1,4 +1,4 @@
-import ProductCard from '../../ProductCard/styles';
+import ProductCard from '../../ProductCard/index';
 import './styles.css';
 
 const Catalogo = () => {
